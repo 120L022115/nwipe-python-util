16cb999ab9c2684a4c4a6653e755d6b5dd33c653
1487819688 <1487819688@qq.com>
2024-01-02 16:36:25 +0800

    test

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ operation.run_nwipe(
     verify=operation.Verifies.last,
     method=operation.Methods.dodshort,
     rounds=10,
-    log_path='/root/nwipe/',
-    pdf_report_path='/root/nwipe/',
+    log_path='/root/nwipe_log/',
+    pdf_report_path='/root/nwipe_log/',
     prng_method=operation.PRNGOption.isaac
 
 )
